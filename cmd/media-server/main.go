7192f89a9d078247fc3b6699fbf0afa310a3ea7e
Jonathan Roth <jonathroth@gmail.com>
2017-07-22 02:18:54 +0300

    Added command line flag for host addr

--- cmd/media-server/main.go
+++ cmd/media-server/main.go
@@ -3,19 +3,25 @@ package main
 import (
 	"net/http"
 
+	"flag"
+
 	"github.com/emicklei/go-restful"
 	"github.com/jonathroth/media-server/lib/controller"
 	"github.com/jonathroth/media-server/lib/serving"
 )
 
+var host = flag.String("h", ":8080", "The server host & port, leave host empty to use all interfaces")
+
 func main() {
+	flag.Parse()
+
 	handler := &controller.WinAPIHandler{}
 	keyboard := &controller.MPCHCPlaybackController{}
 	api := serving.NewMediaPlayerAPI(handler, keyboard)
 
 	container := restful.NewContainer()
 	api.Register(container)
-	err := http.ListenAndServe(":8080", container)
+	err := http.ListenAndServe(*host, container)
 	if err != nil {
 		panic(err)
 	}
